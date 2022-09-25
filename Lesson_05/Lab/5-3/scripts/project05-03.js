70b88d0254c343a2a3476143e68a2e329a834b81
SudoWatson <aulennert@gmail.com>
2022-09-25 15:23:37 -0500

    Week 5

--- Lesson_05/Lab/5-3/scripts/project05-03.js
+++ Lesson_05/Lab/5-3/scripts/project05-03.js
@@ -10,3 +10,27 @@
       Filename: project05-03.js
 */
 
+
+let sourceDoc = document.getElementById("source_doc");
+/** Table of Contents element */
+let toc = document.getElementById("toc");
+let headingCount = 1;
+const HEADING = "H2";
+console.log("Running script");
+for (let childNode = sourceDoc.firstElementChild; childNode !== null; childNode = childNode.nextElementSibling) {
+      console.log("Looping");
+      if (childNode.nodeName === HEADING) {
+            console.log("Found match");
+            let anchor = document.createElement("a");
+            anchor.name = "doclink" + headingCount;
+            childNode.insertBefore(anchor, childNode.firstElementChild);
+            
+            let listItem = document.createElement("li");
+            let link = document.createElement("a");
+            listItem.appendChild(link);
+            link.textContent = childNode.textContent;
+            link.href = "#doclink" + headingCount;
+            toc.appendChild(listItem);
+            headingCount++;
+      }
+}
--- Lesson_05/Lab/5-4/scripts/project05-04.js
+++ Lesson_05/Lab/5-4/scripts/project05-04.js
@@ -13,3 +13,27 @@
 // Node list of phrases that are associated with footnotes
 let phrases = document.querySelectorAll("article blockquote dfn");
 
+for (let i = 0; i < phrases.length; i++) {
+      phrases[i].onclick = (() => {
+            let phrase = document.createElement("h1");
+            phrase.textContent = this.textContent;
+
+            let footnote = document.createElement("p");
+            footnote.textContent = footnotes[i];
+            footnote.style = "font-style: italic; font-size: 1.2em;";
+
+            let closeButton = document.createElement("input");
+            closeButton.type = "button";
+            closeButton.value = "Close Footnote";
+            closeButton.style = "display: block; margin: 10px auto";
+
+            let popup = window.open("", "Footnote", "width=300,height=200,top=100,left=100");
+            let popUpBody = popup.document.body;
+            popUpBody.style =  "background-color: ivory; font-size: 16px; padding: 10px;";
+            popUpBody.appendChild(phrase);
+            popUpBody.appendChild(footnote);
+            popUpBody.appendChild(closeButton);
+            console.log(closeButton);
+            closeButton.onclick = (() => {popup.close();});
+      })
+}
